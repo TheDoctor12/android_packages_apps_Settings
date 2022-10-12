d6b98967d963df4f867ed8bb7946eefda9582538
TheDoctor12 <jkaskabas95@gmail.com>
2022-10-12 21:46:24 +0300

    Revert "Settings: Forward Port Volume Long Press Skip Tracks [2/2]"
    This reverts commit 8579c46c9ade81bb426b987913316c576b5910bf.

--- src/com/android/settings/gestures/GesturesSettingPreferenceController.java
+++ src/com/android/settings/gestures/GesturesSettingPreferenceController.java
@@ -80,7 +80,6 @@ public class GesturesSettingPreferenceController extends BasePreferenceControlle
         controllers.add(new SwipeToScreenshotPreferenceController(context, FAKE_PREF_KEY));
         controllers.add(new ScreenOffUdfpsPreferenceController(context, FAKE_PREF_KEY)
                 .setConfig(ambientDisplayConfiguration));
-        controllers.add(new VolumeButtonMusicControlPreferenceController(context, FAKE_PREF_KEY));
         controllers.add(new PowerButtonTorchGesturePreferenceController(context, FAKE_PREF_KEY));
         return controllers;
     }
